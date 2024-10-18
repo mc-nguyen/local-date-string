b9782beb97f02c0f78b19dd2887c087aff6f7ccd
mc-nguyen <marvin.calvin.1705@gmail.com>
2024-10-17 23:01:05 -0700

    added a function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Returns a new string with the date format of YYYY-MM-DD.
 function getLocalDate(month, day, year) {
   // TODO: Implement this function.
-  const result = 'undefined';
+  const result = `${year}-${month}-${day}`;
   return result;
 }
 
